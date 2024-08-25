428e8b88531722866242a93b11f1ee2d3fe49ab3
SmathyCo <smathyco@gmail.com>
2024-08-25 15:42:20 -0400

    Making a publish.js

--- publish.js
+++ publish.js
@@ -1,22 +1,9 @@
 const { execSync } = require("child_process");
 
 if (process.argv[2]) {
-    let commitMessage = process.argv[2];
-
-    if (/"/.test(commitMessage)) {
-        console.error("Commit message contains double quotes, which is not allowed for security reasons.");
-        process.exit(1);
-    }
-
-    try {
-        execSync("git add -A");
-        execSync(`git commit -m "${commitMessage}"`);
-        execSync("git push -u origin main --force");
-        console.log("Changes pushed successfully.");
-    } catch (error) {
-        console.error("An error occurred:", error.message);
-        process.exit(1);
-    }
+    execSync("git add -A");
+    execSync(`git commit -m "${process.argv[2]}"`);
+    execSync("git push -u origin main --force");
 } else {
     console.log("Please provide a commit message.");
 }
