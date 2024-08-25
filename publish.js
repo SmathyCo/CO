5f70a4fac376a7e4a3464610864bc0131730a343
SmathyCo <smathyco@gmail.com>
2024-08-25 15:51:16 -0400

    Final publish.js test

--- publish.js
+++ publish.js
@@ -2,7 +2,12 @@ const { execSync } = require("child_process");
 
 if (process.argv[2]) {
     execSync("git add -A");
-    execSync(`git commit -m "${process.argv[2]}"`);
+    try {
+        execSync(`git commit -m "${process.argv[2]}"`);
+    } catch(err) {
+        console.error("Error committing changes:", err);
+        process.exit(1);
+    }
     execSync("git push -u origin main --force");
 } else {
     console.log("Please provide a commit message.");
